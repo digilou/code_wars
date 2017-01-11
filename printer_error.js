b004a634c274714628c176b7127b23a8572f8a65
Amy Carney <digilou@users.noreply.github.com>
2017-01-11 11:05:45 -0900

    Added attempt

--- printer_error.js
+++ printer_error.js
@@ -1,14 +1,42 @@
 /*
-In a factory a printer prints labels for boxes. For one kind of boxes the printer has to use colors which, for the sake of simplicity, are named with letters from a to m.
+In a factory a printer prints labels for boxes. For one kind of boxes 
+the printer has to use colors which, for the sake of simplicity, are 
+named with letters from a to m.
 
-The colors used by the printer are recorded in a control string. For example a "good" control string would be aaabbbbhaijjjm meaning that the printer used three times color a, four times color b, then one time color a...
+The colors used by the printer are recorded in a control string. For 
+example a "good" control string would be aaabbbbhaijjjm meaning that the 
+printer used three times color a, four times color b, then one time color a...
 
-Sometimes there are problems: lack of colors, technical malfunction and a "bad" control string is produced e.g. aaaxbbbbyyhwawiwjjjwwm.
+Sometimes there are problems: lack of colors, technical malfunction and 
+a "bad" control string is produced e.g. aaaxbbbbyyhwawiwjjjwwm.
 
-You have to write a function printer_error which given a string will output the error rate of the printer as a string representing a rational whose numerator is the number of errors and the denominator the length of the control string. Don't reduce this fraction to a simpler expression.
+You have to write a function printer_error which given a string will output 
+the error rate of the printer as a string representing a rational whose 
+numerator is the number of errors and the denominator the length of the 
+control string. Don't reduce this fraction to a simpler expression.
 
 The string has a length greater or equal to one and contains only letters from ato z.
 */
 
-// my solution
 
+// my initial thoughts and attempt
+function printerError(s) {
+    // regExp comparison
+    var regex = /\c[a-m]/;
+    // split string into array
+    var s_array = regex.split(s);
+    // placeholder for error messages
+    var error = 0;
+    // iterate thru array to evalute regEx
+    for (var i = 0; i < s_array.length; i++){
+      if (s_array[i] != regex){
+        error += 1;
+      } else{
+        return error;
+      }
+    }
+    // join array back into one string
+      s_array.join("");
+    // return results as a string
+    return error.toString() + "/" + s_array.length.toString();
+}
